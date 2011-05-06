0cdcae125a2b4ba2d90823897b07693812ee1871
Bogdan Gusiev <agresso@gmail.com>
2011-05-06 16:12:02 +0300

    Pivotal commit command

--- lib/pivotal_shell/commands/commit.rb
+++ lib/pivotal_shell/commands/commit.rb
@@ -1,16 +1,51 @@
+require "readline"
+
 module PivotalShell::Commands
   class PivotalShell::Commands::Commit < PivotalShell::Command
-    def initialize(options)
-      @message = options[0]
+    def initialize(arguments)
+
+      opts = OptionParser.new do |opts|
+        opts.banner = "Do git commit with information from pivotal story"
+        opts.on("-m [MESSAGE]", "Add addional MESSAGE to comit") do |message|
+          @message = message
+        end
+      end
+      opts.parse!
+      @story = PivotalShell::Configuration.project.stories.find(arguments.first) unless arguments.empty?
+
     end
 
     def execute
-      puts 'Doesnt work yet! Sorry.'
-      #exit 0 if (message =~ /\[#\d+\]/) || (message =~ /merge/i) # there is already a task ID in the message, or it is a merge
+      if `git diff --staged`.empty?
+        puts "No changed staged to commit."
+        exit 1
+      end
+      stories = PivotalShell::Configuration.project.stories.all( 
+        :owner => PivotalShell::Configuration.me,
+        :state => %w(started finished delivered),
+        :limit => 30
+      )
 
-      #input = File.open('/dev/tty', 'r')
-      
-      #PivotalShell::Configuration.cache.update
+      unless @story
+        unless stories.empty?
+          stories.each_with_index do |story, index|
+            puts "[#{index + 1}] #{story.name}"
+          end
+          puts ""
+          index = Readline.readline("Index: ", true).to_i
+          @story = stories[index - 1]
+
+        end
+      end
+      if @story
+        message <<-MSG
+#{("[\##{@story.id}]").rjust 12} 
+#{@message}\n\n Feature:#{@story.name.strip}
+MSG
+        puts `git commit -m "#{message}"`
+      else
+        puts "Story not found"
+      end
     end
   end
 end
